f60a7b371debca4cb27a647254fc4c02c9c8ea0a
sachin poojary <sachikrishna1997@gmail.com>
2024-08-24 12:28:12 +0530

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn django_banking.wsgi:application --log-file -
--- banking_system/settings.py
+++ banking_system/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -134,10 +137,15 @@ MINIMUM_DEPOSIT_AMOUNT = 10
 MINIMUM_WITHDRAWAL_AMOUNT = 10
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ 
-    BASE_DIR / "static",
-    ]
+#STATICFILES_DIRS = [ 
+   # BASE_DIR / "static",
+  #  ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+django_heroku.settings(locals())
 # Celery Settings
 CELERY_BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
